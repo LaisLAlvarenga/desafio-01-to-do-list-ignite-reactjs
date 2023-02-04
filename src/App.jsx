b527e8c356d2ede533f9d1de7d6a087dfff313c6
Lais Luderer Alvarenga <lais.alvarenga.global@gmail.com>
2023-02-04 01:34:45 -0300

    feat: Implementar e estilizar a Task.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {PlusCircle} from "phosphor-react";
+import {PlusCircle, Trash} from "phosphor-react";
 import "./global.css";
 import styles from './App.module.css';
 import Logo from './assets/logo.svg';
@@ -10,11 +10,11 @@ function App() {
         <img src={Logo} alt="Logomarca do ToDo" />
       </header>
 
-      <form className={styles.form}>
+      <form className={styles.inputTasks}>
         <input 
             type="text" 
-            name="task" 
-            id="task" 
+            name="inputTask" 
+            id="inputTask" 
             placeholder='Adicione uma nova tarefa' 
         />
         <button type="submit" >
@@ -31,9 +31,39 @@ function App() {
 
         <div>
             <strong className={styles.conclued}>Concluídas</strong>
-            <span>2 de 3</span>
+            <span>1 de 3</span>
         </div>
       </div>
+
+      <div className={styles.taskContainer}>
+        <div className={styles.tasks}>
+            <input type="checkbox" name="checkboxTask" id="checkboxTask" />
+            <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+        </div>
+        <button className={styles.trashIcon}>
+            <Trash size={20} />
+        </button>
+    </div>
+
+    <div className={styles.taskContainer}>
+        <div className={styles.tasks}>
+            <input type="checkbox" name="checkboxTask" id="checkboxTask" />
+            <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+        </div>
+        <button className={styles.trashIcon}>
+            <Trash size={20} />
+        </button>
+    </div>
+
+    <div className={styles.taskContainer}>
+        <div className={styles.tasks}>
+            <input type="checkbox" name="checkboxTask" id="checkboxTask" checked/>
+            <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+        </div>
+        <button className={styles.trashIcon}>
+            <Trash size={20} />
+        </button>
+    </div>
     </>
   )
 }
